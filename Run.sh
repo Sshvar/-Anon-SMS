dbeff5d65f5fbeb3e528c016dcda49cb9f62ee0b
ssh.var <97916240+Sshvar@users.noreply.github.com>
2022-03-24 00:20:20 -0400

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 clear
-echo -e "\e[4;31m Sshvar !!! \e[0m"
+echo -e "\e[4;31m Panda Hackers !!! \e[0m"
 echo -e "\e[1;32m Presents \e[0m"
 echo -e "\e[1;34m"
 figlet -f slant Anon-SMS
 echo "Press Enter To Launch"
 read a1
-if [[ -s update.Sshvar ]];then
+if [[ -s update.pandahackers ]];then
 echo "All Requirements Found...."
 else
 echo 'Installing Requirements....'
@@ -16,8 +16,8 @@ echo .
 apt install figlet toilet python curl ruby -y
 apt install python3-pip
 gem install lolcat
-echo This Tool Is Created By Sshvar
-echo This Script Is Created By Sshvar >update.Sshvar
+echo This Tool Is Created By HACK3RY2J
+echo This Script Is Created By Panda Hackers HACK3RY2J >update.pandahackers
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -30,11 +30,11 @@ echo -e "\e[1;31m"
 figlet -f slant Anon-SMS | lolcat
 echo -e "\e[1;34m Created By \e[1;32m"  #blue color
 toilet -f mono12 -F border PH | lolcat
-echo -e "\e[4;34mThis Tool Is Created By Sshvar \e[0m" #underline+blue
+echo -e "\e[4;34mThis Tool Is Created By Panda Hackers \e[0m" #underline+blue
 echo -e "\e[1;34mFor Any Queries Mail Us!!!\e[0m"
-echo -e "\e[1;32m        Mail: Hackerscracker6@gmail.com \e[0m" #yellow
-echo -e "\e[4;32mYouTube Page: https://www.youtube.com/channel/UCTrXHB_H8u_9WRc5oX5w0Wg/ \e[0m" #green
-echo -e "\e[1;34m    Telegram: https://t.me/Sshvar \e[0m"
+echo -e "\e[1;32m        Mail: Hack3ry2j@gmail.com \e[0m" #yellow
+echo -e "\e[4;32mYouTube Page: https://youtube.com/c/PandaHackers/ \e[0m" #green
+echo -e "\e[1;34m    Telegram: https://t.me/ItzAnonSMS \e[0m"
 echo " "
 echo -e "\e[4;31mRead Instruction Carefully !!! \e[0m" #red
 echo " "
@@ -64,13 +64,13 @@ apt install git -y
 echo -e "\e[1;34m Requesting Update From Source..."
 echo -e "\e[1;34m Request Attempt Successful.."
 echo -e "\e[1;34m Updating Now..."
-git clone https://github.com/Sshvar/Anon-SMS.git
+git clone https://github.com/HACK3RY2J/Anon-SMS.git
 if [[ -s Anon-SMS/Run.sh ]];then
 cd Anon-SMS
 cp -r -f * .. > temp
 cd ..
 rm -rf  Anon-SMS >> temp
-rm update.Sshvar >> temp
+rm update.pandahackers >> temp
 rm temp
 chmod +x Run.sh
 fi
@@ -96,15 +96,15 @@ echo "  Easy To Use And Embed in Code"
 echo""
 echo -e "\e[1;32m                   Contributors\e[1;33m"
 echo -e "\e[1;33m      [*]  Panda Hackers   \e[1;31m"
-echo "         [-] Mail At: hackerscracker6@gmail.com"
+echo "         [-] Mail At: pandahacker127@gmail.com"
 echo -e "\e[1;33m      [*]  Instagram   \e[1;31m"
 echo "         [-] Ping At: http://Instagram.com/"
 echo -e "\e[1;33m      [*]  Telegram   \e[1;31m"
-echo "         [-] Ping At: https://t.me/Sshvar/"
-echo -e "\e[1;33m      [*]  Sshvar   \e[1;31m"
-echo "         [-] Mail At: HACHERSCRACKER6@gmail.com"
-echo -e "\e[1;33m      [*]  SSH.VAR      \e[1;31m"
-echo "         [-] Ping At: https://www.instagram.com/ssh.var/"
+echo "         [-] Ping At: https://t.me/ItzAnonSMS/"
+echo -e "\e[1;33m      [*]  HACK3RY2J   \e[1;31m"
+echo "         [-] Mail At: HACK3RY2J@gmail.com"
+echo -e "\e[1;33m      [*]  HAOI      \e[1;31m"
+echo "         [-] Ping At: http://Instagram.com/haoi_hackers_Academy_of_India/"
 echo ""
 echo ""
 echo -e "\e[1;31m This is Only For Educational Purposes or To Prank.\e[0m"
@@ -115,9 +115,9 @@ echo -e "\e[1;32m Update If Doesn't Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Us!!!\e[0m"
-echo -e "\e[1;32m        Mail: HACKERSCRACKER6@gmail.com \e[0m"
+echo -e "\e[1;32m        Mail: HACK3RY2J@gmail.com \e[0m"
 echo -e "\e[1;32m   Instagram: https://Instagram.com/ \e[0m"
-echo -e "\e[4;32mYouTube Page: https://www.youtube.com/channel/UCTrXHB_H8u_9WRc5oX5w0Wg \e[0m"
+echo -e "\e[4;32mYouTube Page: https://www.youtube.com/c/PandaHackers \e[0m"
 echo "Press Enter To Return To Main Menu"
 read a3
 clear
@@ -126,11 +126,11 @@ clear
 echo -e "\e[1;31m"
 figlet -f slant Anon-SMS | lolcat
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f pagga -F border Sshvar | lolcat
+toilet -f pagga -F border HACK3RY2J | lolcat
 echo -e "\e[1;34m For Any Queries Mail Us!!!\e[0m"
-echo -e "\e[1;32m        Mail: HACKERSCRACKER6@gmail.com \e[0m"
-echo -e "\e[1;32m    Telegram: https://t.me/Sshvar \e[0m"
-echo -e "\e[4;32mYouTube Page: https://www.youtube.com/channel/UCTrXHB_H8u_9WRc5oX5w0Wg/ \e[0m"
+echo -e "\e[1;32m        Mail: HACK3RY2J@gmail.com \e[0m"
+echo -e "\e[1;32m    Telegram: https://t.me/ItzAnonSMS \e[0m"
+echo -e "\e[4;32mYouTube Page: https://www.youtube.com/c/PandaHackers/ \e[0m"
 echo " "
 exit 0
 else
@@ -140,3 +140,4 @@ read a3
 clear
 fi
 done
+
